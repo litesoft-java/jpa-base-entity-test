4b4c7e42c8b2b9d93546063f2d7b278007bea43e
george.smith <litesoft@gmail.com>
2022-09-28 18:29:34 -0700

    Cleaned up the code (format and imports), and Bump Versions

--- src/main/java/org/litesoft/baseentity/BaseEntityHelper.java
+++ src/main/java/org/litesoft/baseentity/BaseEntityHelper.java
@@ -5,6 +5,6 @@ package org.litesoft.baseentity;
  */
 public class BaseEntityHelper {
     public static <T extends AbstractBaseEntity<T>> void setVersion( T entity, Long version ) {
-        entity.setVersion(version);
+        entity.setVersion( version );
     }
 }
