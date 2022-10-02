92fc6981464d985c1352adbebfdaf72deae26598
george.smith <litesoft@gmail.com>
2022-10-02 09:34:56 -0700

    Bump Versions

--- src/main/java/org/litesoft/baseentity/BaseEntityHelper.java
+++ src/main/java/org/litesoft/baseentity/BaseEntityHelper.java
@@ -3,6 +3,7 @@ package org.litesoft.baseentity;
 /**
  * Backdoor to set version number for Entities using "jpa-base-entity" library.
  */
+@SuppressWarnings("unused")
 public class BaseEntityHelper {
     public static <T extends AbstractBaseEntity<T>> void setVersion( T entity, Long version ) {
         entity.setVersion( version );
